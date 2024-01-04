965da232ddc8c408a80d1073e2c613bee38e6a09
kote <kot123bog@gmail.com>
2024-01-04 13:02:33 +0200

    Removed sever folder from repository

--- server/Models/ProductImage.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const ProductImage = new mongoose.Schema({
-    base64: String,
-    imageFormat: String
-})
-
-export default mongoose.model("ProductImage", ProductImage);
